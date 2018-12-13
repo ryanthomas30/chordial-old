f442956fcaa315c3e8c0902dcfd32842abd724d4
Ryan Thomas <ryanthomas30@gmail.com>
2018-12-13 01:39:58 -0500

    Restructure state

--- src/js/components/App.js
+++ src/js/components/App.js
@@ -1,98 +1,113 @@
 import React, { Component } from 'react';
 
-import { Header, Form, TextArea } from 'semantic-ui-react';
+import { Header, Form, TextArea, Modal, Button, Input } from 'semantic-ui-react';
 
 import MainHeader from './MainHeader';
 import Stanza from './Stanza';
 import FlexBox from './custom/FlexBox';
+import { example } from './example';
+
+function generateStanzaArray(originalLyrics) {
+	if (!originalLyrics) return [];
+	/* Array of stanzas */
+	/* Stanza is an array of lines */
+	/* Line is array of chars */
+	const stanzaArray = [];
+	let currentStanza = [];
+	let currentLine = [];
+
+	const textArray = originalLyrics.split('');
+	for (let i = 0; i < originalLyrics.length; i++) {
+		// Line Break
+		if (textArray[i] === '\n') {
+			// Double Line Break; End of Stanza & Line
+			if (textArray[i + 1] === '\n' || textArray[i + 1] === undefined) {
+				console.log('currentLine:', currentLine);
+				console.log('currentStanza:', currentStanza);
+				console.log('textArray[i + 1]:', textArray[i + 1]);
+				// Push line of spaces before pushing current line
+				currentStanza.push(currentLine.map(c => ' '));
+				currentStanza.push(currentLine);
+				stanzaArray.push(currentStanza);
+				currentStanza = [];
+				currentLine = [];
+				// Skip next line
+				i++;
+				// Single Line Break; End of Line
+			} else {
+				console.log('currentLine:', currentLine);
+				// Push line of spaces before pushing current line
+				currentStanza.push(currentLine.map(c => ' '));
+				currentStanza.push(currentLine);
+				currentLine = [];
+			}
+			// Final Char; End of Song & Stanza & Line
+		} else if (textArray[i + 1] === undefined) {
+			// Push line of spaces before pushing current line
+			currentStanza.push(currentLine.map(c => ' '));
+			currentLine.push(textArray[i]);
+			currentStanza.push(currentLine);
+			stanzaArray.push(currentStanza);
+			currentStanza = [];
+			currentLine = [];
+			// Regular Char
+		} else {
+			console.log('textArray[i]:', textArray[i]);
+			currentLine.push(textArray[i]);
+		}
+	}
+	return stanzaArray;
+}
 
 class App extends Component {
 	constructor(props) {
 		super(props);
 
-		this._onInputChange = this._onInputChange.bind(this);
-
 		this.state = {
-			originalText: ''
+			originalLyrics: example,
+			title: 'Hakuna Muh Potato',
+			stanzaArray: generateStanzaArray(example),
+			modalOpen: false
 		};
 	}
 
-	_onInputChange(e) {
-		this.setState({ originalText: e.target.value });
-	}
-
 	render() {
-		const { originalText } = this.state;
-		console.log('originalText:', originalText);
-		const wasNewline = false;
-		const displayText = originalText.split('').map(c => {
-			switch (c) {
-				case '\n':
-					if (wasNewline) {
-						// wasNewline = false;
-						return (
-							<span>&nbsp;</span>
-						);
-					}
-					// wasNewline = true;
-					return (
-						<React.Fragment>
-							<br/><br/>
-						</React.Fragment>
-					);
-				default:
-					// wasNewline = false;
-					return <span className='character' >{c}</span>;
-			}
-		});
-		displayText.unshift(
-			<React.Fragment>
-				<br/><br/>
-			</React.Fragment>
+		let { originalLyrics, title } = this.state;
+		const { stanzaArray } = this.state;
+		title = title || 'No Title';
+		const textInputButton = () => (
+			<Modal
+				trigger={<Button primary onClick={() => this.setState({ modalOpen: true, lyricsInput: originalLyrics, titleInput: title })}>Enter Lyrics</Button>}
+				open={this.state.modalOpen}
+				onClose={() => this.setState({ modalOpen: false })}
+			>
+				<Modal.Header>Lyrics</Modal.Header>
+				<Modal.Content>
+					<Modal.Description>
+						<Form>
+							<Input style={{ marginBottom: '24px' }} defaultValue={title} onChange={(e) => this.setState({ titleInput: e.target.value })} />
+							<TextArea autoHeight style={{ marginBottom: '24px', overflow: 'hidden' }} defaultValue={originalLyrics} onChange={(e) => this.setState({ lyricsInput: e.target.value })} />
+							<Button
+								type='submit'
+								content='Submit'
+								primary
+								onClick={ () => this.setState({
+									originalLyrics: this.state.lyricsInput,
+									title: this.state.titleInput,
+									modalOpen: false,
+									stanzaArray: generateStanzaArray(this.state.lyricsInput) })
+								}
+							/>
+						</Form>
+					</Modal.Description>
+				</Modal.Content>
+			</Modal>
 		);
-		/* Array of stanzas */
-		/* Stanza is an array of lines */
-		/* Line is array of chars */
-		const stanzaArray = [];
-		let currentStanza = [];
-		let currentLine = [];
-		const textArray = originalText.split('');
-		for (let i = 0; i < originalText.length; i++) {
-			// Line Break
-			if (textArray[i] === '\n') {
-				// Double Line Break; End of Stanza
-				if (textArray[i + 1] === '\n' || textArray[i + 1] === undefined) {
-					console.log('currentLine:', currentLine);
-					console.log('currentStanza:', currentStanza);
-					console.log('textArray[i + 1]:', textArray[i + 1]);
-					currentStanza.push(currentLine);
-					stanzaArray.push(currentStanza);
-					currentStanza = [];
-					currentLine = [];
-					i++;
-				// Single Line Break; End of Line
-				} else {
-					console.log('currentLine:', currentLine);
-					currentStanza.push(currentLine);
-					currentLine = [];
-				}
-			} else if (textArray[i + 1] === undefined) {
-				currentStanza.push(currentLine);
-				stanzaArray.push(currentStanza);
-				currentStanza = [];
-				currentLine = [];
-			} else {
-				console.log('textArray[i]:', textArray[i]);
-				currentLine.push(textArray[i]);
-			}
-		}
-
-		console.log('stanzaArray:', stanzaArray);
 
 		const Stanzas = () => {
-			return stanzaArray.map(stanza => {
+			return stanzaArray.map((stanza, i) => {
 				return (
-					<Stanza stanza={stanza} />
+					<Stanza stanza={stanza} key={i} />
 				);
 			});
 		};
@@ -101,12 +116,10 @@ class App extends Component {
 			<div className='app' >
 				<MainHeader />
 				<div className='main-content' >
+					{textInputButton()}
 					<Header as='h1' >
-						Hakuna Mungu Kama Wewe
+						{title}
 					</Header>
-					<Form>
-						<TextArea autoHeight onChange={(e) => this._onInputChange(e)} />
-					</Form>
 					<Stanzas />
 				</div>
 			</div>
--- src/js/components/Stanza.js
+++ src/js/components/Stanza.js
@@ -3,17 +3,34 @@ import React, { Component } from 'react';
 class Stanza extends Component {
 	render() {
 		const { stanza } = this.props;
-		const renderStanza = stanza.map(line => {
-			const displayLine = line.map(c => {
-				return (
-					<span className='character' >
-						{c}
-					</span>
-				);
-			});
+		const renderStanza = stanza.map((line, i) => {
+			let displayLine = '';
+			// ODD: Lyric Line
+			if (i % 2 !== 0) {
+				displayLine = line.map((c, j) => {
+					return (
+						<span className='character' key={j} >
+							{c}
+						</span>
+					);
+				});
+			// EVEN: Chord Line
+			} else {
+				displayLine = line.map((c, j) => {
+					console.log('c:', c);
+					return (
+						<span className='character' key={j} >
+							{c === ' ' ? <React.Fragment>&nbsp;</React.Fragment> : c }
+						</span>
+					);
+				});
+			}
 			displayLine.push(<br />);
-			displayLine.push(<br />);
-			return displayLine;
+			return (
+				<React.Fragment>
+					{displayLine}
+				</React.Fragment>
+			);
 		});
 		renderStanza.unshift(<br />);
 
